7865dee68b34eec7f7112ec752b7902179250b8a
wdmuthiora <wdmuthiora@gmail.com>
2021-11-29 01:45:24 +0300

    Designed the Main Activity Layout.

--- app/src/main/java/com/moringaschool/android_ip_1/Activity/MainActivity.java
+++ app/src/main/java/com/moringaschool/android_ip_1/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.moringaschool.android_ip_1;
+package com.moringaschool.android_ip_1.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.moringaschool.android_ip_1.R;
 
 public class MainActivity extends AppCompatActivity {
 
