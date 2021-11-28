056ab97b868470a6a6c7cdcc25bf730b74aa091e
wdmuthiora <wdmuthiora@gmail.com>
2021-11-29 01:51:11 +0300

    Added MainActivity2, and added an event listener to navigate to MainActivity2.

--- app/src/main/java/com/moringaschool/android_ip_1/Activity/MainActivity.java
+++ app/src/main/java/com/moringaschool/android_ip_1/Activity/MainActivity.java
@@ -1,16 +1,29 @@
 package com.moringaschool.android_ip_1.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.moringaschool.android_ip_1.R;
 
 public class MainActivity extends AppCompatActivity {
+    private ConstraintLayout startBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startBtn=findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+            }
+        });
     }
 }
