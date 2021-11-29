803e1091a101e9a3f7fcb56cbfd12acdaf7046de
wdmuthiora <wdmuthiora@gmail.com>
2021-11-30 01:56:12 +0300

    Re-Styled the MainActivity.

--- app/src/main/java/com/moringaschool/android_ip_1/Activity/Activity/MainActivity.java
+++ app/src/main/java/com/moringaschool/android_ip_1/Activity/Activity/MainActivity.java
@@ -6,7 +6,9 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.moringaschool.android_ip_1.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,20 +16,36 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.startBtn) ConstraintLayout startBtn;
-    @BindView(R.id.userName) ConstraintLayout userName;
-
+    @BindView(R.id.userName) TextInputEditText userName;
+    @BindView(R.id.welcomeTxt) TextView welcomeTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         ButterKnife.bind(this);
+
+//        if(userName.getText().toString().isEmpty()){
+//            startBtn.setVisibility(View.GONE);
+//            welcomeTxt.setVisibility(View.GONE);
+//        }else{
+//            startBtn.setVisibility(View.VISIBLE);
+//            welcomeTxt.setVisibility(View.VISIBLE);
+//        }
 
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+
+                //capture user input
+                String inputUserName = userName.getText().toString();
+
+                Intent intent=new Intent(MainActivity.this, MainActivity2.class);
+                //pass the captured input to the next Activity
+                intent.putExtra("inputUserName", inputUserName);
+                startActivity(intent);
             }
         });
     }
